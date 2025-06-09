cc74e1b9db05bb191546c2c1cce28e8a6ecaadc1
danesdeova01 <151486791+danesdeova01@users.noreply.github.com>
2025-06-09 08:19:17 +0700

    Update Utility.java

--- src/test/java/helper/Utility.java
+++ src/test/java/helper/Utility.java
@@ -1,20 +1,30 @@
 package helper;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import javax.sound.midi.Soundbank;
 
 public class Utility {
 
     public static WebDriver webDriver;
 
 
-    //Membuka Google Chrome
-    public static void startWebDriver(){
+
+    @Before
+    public static void setUp(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); //For CI CD running
+
+        //Setup chromedriver via WebDriverManager
         WebDriverManager.chromedriver().setup();
-        webDriver = new ChromeDriver();
-        webDriver.manage().window().fullscreen();
-        webDriver.get("https://www.saucedemo.com/");
+        System.out.println("Intializing WebDriver...");
+        webDriver = new ChromeDriver(options);
+        System.out.println("WebDriver initialized successfully");
+
     }
 
     //Menutup Chrome
